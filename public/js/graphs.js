f29370356ace4299bbcfe66aad7fd247c516408d
marcio <marciodscampos@gmail.com>
2017-12-08 16:21:30 -0200

    ploting data

--- public/js/graphs.js
+++ public/js/graphs.js
@@ -1,5 +1,5 @@
 function scatterPlot(){
-    var trace1 = {
+    /*var trace1 = {
         x: [1, 2, 3, 4],
         y: [10, 15, 13, 17],
         mode: 'markers'
@@ -11,18 +11,29 @@ function scatterPlot(){
         mode: 'lines'
     };
 
-    var trace3 = {
+    /*var trace3 = {
         x: [1, 2, 3, 4],
         y: [12, 9, 15, 12],
         mode: 'lines+markers'
-    };
+    };*/
 
-    var data = [trace1, trace2, trace3];
+    var data = [{
+        x: [],
+        y: [],
+        type: 'bar'
+    }];
+
+    data2017.forEach(function(item){
+        data[0].x.push(item.Country);
+        data[0].y.push(item.Happiness_Rank);
+    })
+
+   
 
     var layout = {
         title: 'Line and Scatter Plot',
         autosize: true
     };
 
-    Plotly.newPlot('myDiv', data, layout);
+    Plotly.newPlot('myDiv', data);
 }
