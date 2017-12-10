5f48f1f44efcbbfd9c300f71b68a0e977b130189
marciocampos <marciodscampos@gmail.com>
2017-12-10 21:23:10 +0000

    added plots

--- public/js/graphs.js
+++ public/js/graphs.js
@@ -1,22 +1,12 @@
+function getColumn(data, column_name){
+    var column = [];
+    data.forEach(function(item){
+        column.push(item[column_name]);
+    })
+    return column;
+}
+
 function scatterPlot(){
-    /*var trace1 = {
-        x: [1, 2, 3, 4],
-        y: [10, 15, 13, 17],
-        mode: 'markers'
-    };
-
-    var trace2 = {
-        x: [2, 3, 4, 5],
-        y: [16, 5, 11, 10],
-        mode: 'lines'
-    };
-
-    /*var trace3 = {
-        x: [1, 2, 3, 4],
-        y: [12, 9, 15, 12],
-        mode: 'lines+markers'
-    };*/
-
     var data = [{
         x: [],
         y: [],
@@ -25,10 +15,8 @@ function scatterPlot(){
 
     data2017.forEach(function(item){
         data[0].x.push(item.Country);
-        data[0].y.push(item.Happiness_Rank);
-    })
-
-   
+        data[0].y.push(item.Happiness_Score);
+    });
 
     var layout = {
         title: 'Line and Scatter Plot',
@@ -36,4 +24,136 @@ function scatterPlot(){
     };
 
     Plotly.newPlot('myDiv', data);
+}
+
+function scatterMatrixPlot(){
+    var columns = ["Economy_GDP", "Health_Life_Expectancy","Freedom"];
+    var interval = 0.9/columns.length;
+    var space = 0.1/columns.length;
+    var data = [];
+    var layout = {
+        title: 'Matriz Scatterplot',
+        xaxis: { domain: [0, interval - space] },
+        yaxis: { domain: [1 - interval + space, 1], title: columns[0] },
+        showlegend: false,
+        //autosize: false,
+        height: 900
+    }
+
+    for(var i = 0; i < columns.length; i++){
+        for(var j = 0; j < columns.length; j++){
+            var trace = {
+                x: getColumn(data2017, columns[i]),
+                y: getColumn(data2017, columns[j]),
+                mode: 'markers',
+                type: 'scatter',
+                text: getColumn(data2017, "Country"),
+                showtext: false,
+                marker: {
+                    //size: 40,
+                    color: getColumn(data2017, "Happiness_Score"),
+                    autocolorscale: false,
+                    showscale: true
+                },
+                //name: getColumn(data2017, "Country")
+            }
+            var count = (columns.length * i) + j + 1;
+            if(count != 1){
+                trace['xaxis'] = 'x' + count;
+                trace['yaxis'] = 'y' + count;
+                layout['xaxis' + count] = {
+                    domain: [j*interval, (j+1) * interval - space],
+                    anchor: 'y' + count
+                };
+                layout['yaxis' + count] = {
+                    domain: [1 - ((i + 1) * interval) + space, 1 - (i * interval)],
+                    anchor: 'x' + count
+                };
+            }
+            if(j == 0 && count != 1){
+                layout['yaxis' + count].title = columns[i];
+            }
+            if(i == columns.length-1){
+                layout['xaxis' + count].title = columns[j];                
+            }
+            data.push(trace);
+        }
+    }
+    console.log(data);
+    Plotly.newPlot('scatterMatrixDiv', data, layout);
+}
+
+function mapPlot(){
+    var data = [{
+        type: 'choropleth',
+        locationmode: 'country names',
+        locations: [],
+        z: [],
+        text: [],
+        autocolorscale: false
+    }];
+
+    data2017.forEach(function(item){
+        data[0].locations.push(item.Country);
+        data[0].text.push(item.Country);
+        data[0].z.push(item.Happiness_Score);
+    });
+
+    var layout = {
+        title: 'Mapa do coeficiente de felicidade',
+        geo: {
+            projection: {
+                type: 'robinson'
+            }
+        },
+        height: 600
+    };
+
+    Plotly.plot(mapDiv, data, layout, { showLink: false });
+}
+
+function parallelPlot(){
+    getColumn(data2017, "Country");
+    var data = [{
+        type: 'parcoords',
+        pad: [80, 80, 80, 80],
+        line: {
+            color: [],
+            colorscale: 'Rainbow',
+            showscale: true
+        },
+
+        dimensions: [{
+            range: [0, 10],
+            label: 'Happiness Score',
+            values: getColumn(data2017, "Happiness_Score")
+        }, {
+            constraintrange: [5, 6],
+            range: [0, 2],
+            label: 'Economy GDP',
+            values: getColumn(data2017, "Economy_GDP")
+        }, {
+            label: 'Life Expectancy',
+            range: [0, 1],
+            values: getColumn(data2017, "Health_Life_Expectancy")
+        }, {
+            label: 'Freedom',
+            range: [0, 1],
+            values: getColumn(data2017, "Freedom")
+        },
+        {
+            label: 'Generosity',
+            range: [0, 1],
+            values: getColumn(data2017, "Generosity")
+        }]
+    }];
+
+    data2017.forEach(function(item){
+        var continent = countries[item.Country].Continent;
+        data[0].line.color.push(getContinentIndex(continent));
+    });
+
+    var layout = { title: 'Coordenadas paralelas' };
+
+    Plotly.plot('parallelDiv', data, layout);
 }
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,6 +2,7 @@
 var data2015 = [];
 var data2016 = [];
 var data2017 = [];
+var countries = {};
 
 // Functions
 
@@ -22,6 +23,62 @@ function getData(year, list) {
         data.forEach(function(item){
             list.push(item);
         });
+    });
+}
+
+function getCountries(){
+    $.getJSON("data/countries.json", function (data) {
+        countries = data;
+    });
+}
+
+function getContinentName(continent_code){
+    var option = {
+        "AF": "Africa",
+        "AN": "Antarctica",
+        "AS": "Asia",
+        "EU": "Europe",
+        "NA": "North america",
+        "OC": "Oceania",
+        "SA": "South america"
+    }
+    return option[continent_code];
+}
+
+function getContinentIndex(continent_code) {
+    var option = {
+        "AF": 0,
+        "AN": 3, // same as europe
+        "AS": 1,
+        "EU": 3,
+        "NA": 2,
+        "OC": 4,
+        "SA": 2
+    }
+    return option[continent_code];
+}
+
+// PRE-PROCESS ONLY get a code -> country object and turn into a country -> code
+function convertCodeCountry(next){
+    $.getJSON("data/code_country.json", function (data) {
+        Object.keys(data).forEach(function (key) {
+            countries[data[key]] = key;
+        });
+        next();
+    });
+}
+
+// PRE-PROCESS ONLY join the country data to it's continent
+function country_continent(){
+    $.getJSON("data/country_code_continent.json", function (data) {
+        Object.keys(countries).forEach(function(country_name){
+            var country_code = countries[country_name];
+            countries[country_name] = {
+                "code": country_code,
+                "Continent": data[country_code]
+            };
+        });
+        console.log(JSON.stringify(countries));
     });
 }
 
@@ -48,7 +105,10 @@ $(window).resize(function () {
 // Run when the page finishes loading
 $(document).ready(function () {
     toggleView();
+    $("#content").load("/description.html");
     getData("2015", data2015);
     getData("2016", data2016);
     getData("2017", data2017);
+    getCountries();
+    // convertCodeCountry(country_continent); // Used when pre processing. Result is saved as countries.json
 });
