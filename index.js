4bb14ee1f0fddce9702bd7ab32513a734c57db47
marciocampos <marciodscampos@gmail.com>
2017-12-10 21:46:55 +0000

    port fix

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 
+var server_port = process.env.PORT || 3000;
+var server_host = '0.0.0.0';
 //app.get('/', (req, res) => res.send('Hello World!'))
 
 app.use(express.static('public'))
 
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(server_port, server_host, () => console.log('Example app listening on port %d!', server_port))
